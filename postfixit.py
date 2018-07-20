7ae8da67f4bc0452c4e11f5d46432ac38f1d25d5
zhoushuaiandrew981112 <32692011+zhoushuaiandrew981112@users.noreply.github.com>
2018-07-19 18:25:08 -0700

    Update postfixit.py

--- postfixit.py
+++ postfixit.py
@@ -6,6 +6,35 @@
 
 class Stack:
     
+    def __init__(self, capacity):
+        if capacity < 0:
+            raise ValueError
+        else:
+            self.capacity = capacity
+            self.items = [None] * capacity
+            self.num_item = 0
+  
+
+    def push(self, item):
+        if self.num_item >= self.capacity:
+            raise ValueError
+        else:
+            self.items[self.num_item] = item
+            self.num_item += 1
+
+
+    def pop(self):
+        if self.num_item <= 0:
+            raise ValueError
+        else:
+            self.num_item -= 1
+            temp = self.items[self.num_item]
+            self.items[self.num_item] = None
+            return temp
+
+    def peek(self):
+          return self.items[self.num_item - 1]
+    
  
 
 def prec(char):
